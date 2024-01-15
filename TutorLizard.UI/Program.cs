6bff0e67915ccf6c69e27dd039246128ec240c6d
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-15 17:29:27 +0100

    Add solution, gitignore, UI and BusinessLogic project files

---
+++ TutorLizard.UI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TutorLizard.UI;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Tutor Lizard - Korepetycje z Jaszczurem");
+        Console.ReadLine();
+    }
+}
