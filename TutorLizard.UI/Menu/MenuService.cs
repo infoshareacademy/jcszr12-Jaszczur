19315a1ed0272f23b3d0ad85af4b73e8f2ce196b
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-16 13:09:26 +0100

    Add MenuService.cs to UI
    This class will handle showing interactive menus to the user and activating requested functionalities. It needs access to UserIdentityService, TutorService and StudentService in order to be able to activate their functionalities.

---
+++ TutorLizard.UI/Menu/MenuService.cs
@@ -0,0 +1,23 @@
+﻿using TutorLizard.BusinessLogic.Data;
+using TutorLizard.BusinessLogic.Services;
+
+namespace TutorLizard.UI.Menu;
+public class MenuService
+{
+    private readonly UserIdentityService _userIdentityService;
+    private readonly TutorService _tutorService;
+    private readonly StudentService _studentService;
+
+    public MenuService(UserIdentityService userIdentityService, TutorService tutorService, StudentService studentService)
+    {
+        _userIdentityService = userIdentityService;
+        _tutorService = tutorService;
+        _studentService = studentService;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Tutor Lizard - Korepetycje z Jaszczurem");
+        Console.ReadLine();
+    }
+}
