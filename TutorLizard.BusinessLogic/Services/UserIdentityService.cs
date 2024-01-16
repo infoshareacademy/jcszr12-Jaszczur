120ccb0adea70a4b33aaf1a72406ff4868533bae
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-16 12:57:28 +0100

    Add UserIdentityService.cs to BusinessLogic
    This class will keep track of user information and their permissions. It needs access to DataAccess class in order to load and save user information.

---
+++ TutorLizard.BusinessLogic/Services/UserIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using TutorLizard.BusinessLogic.Data;
+
+namespace TutorLizard.BusinessLogic.Services;
+public class UserIdentityService
+{
+    private readonly DataAccess _dataAccess;
+
+    public UserIdentityService(DataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+}
