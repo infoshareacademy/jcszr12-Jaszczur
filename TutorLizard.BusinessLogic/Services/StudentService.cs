f5783855e727261709eece605f2a05d37885075b
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-16 13:02:52 +0100

    Add StudentService.cs to BusinessLogic
    This class will handle students' functionality. It needs access to DataAccess in order to load and save data and access to UserIdentityService in order to verify what data and funcionality is available to the user.

---
+++ TutorLizard.BusinessLogic/Services/StudentService.cs
@@ -0,0 +1,14 @@
+﻿using TutorLizard.BusinessLogic.Data;
+
+namespace TutorLizard.BusinessLogic.Services;
+public class StudentService
+{
+    private readonly DataAccess _dataAccess;
+    private readonly UserIdentityService _userIdentityService;
+
+    public StudentService(DataAccess dataAccess, UserIdentityService userIdentityService)
+    {
+        _dataAccess = dataAccess;
+        _userIdentityService = userIdentityService;
+    }
+}
