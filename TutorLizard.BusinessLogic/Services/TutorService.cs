191a4110e26a766291fa5ee6797757ef8dc01473
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-16 13:03:39 +0100

    Add TutorService.cs to BusinessLogic
    This class will handle tutors' functionality. It needs access to DataAccess in order to load and save data and access to UserIdentityService in order to verify what data and funcionality is available to the user.

---
+++ TutorLizard.BusinessLogic/Services/TutorService.cs
@@ -0,0 +1,14 @@
+﻿using TutorLizard.BusinessLogic.Data;
+
+namespace TutorLizard.BusinessLogic.Services;
+public class TutorService
+{
+    private readonly DataAccess _dataAccess;
+    private readonly UserIdentityService _userIdentityService;
+
+    public TutorService(DataAccess dataAccess, UserIdentityService userIdentityService)
+    {
+        _dataAccess = dataAccess;
+        _userIdentityService = userIdentityService;
+    }
+}
