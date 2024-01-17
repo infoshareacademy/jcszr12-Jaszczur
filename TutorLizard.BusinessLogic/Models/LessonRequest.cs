ab2f9f95ce15e0deb1dc3a1b42b8342c98b5101f
skrawus <wudarskap@gmail.com>
2024-01-17 20:44:12 +0100

    Add structure of lesson and ad requests.

---
+++ TutorLizard.BusinessLogic/Models/LessonRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorLizard.BusinessLogic.Models;
+
+public class LessonRequest
+{
+    public int Id { get; set; }
+    public int AdId { get; set; }
+    public int ScheduleId { get; set; }
+    public int UserId { get; set; }
+    public bool IsAccepted {  get; set; }
+}
