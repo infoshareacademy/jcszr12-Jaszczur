4a25f42748e4d0d60fb2c8191ce02525be267eb9
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-23 11:49:26 +0100

    Add MainMenuScreen.cs
    It will be the entry point of our application.

---
+++ TutorLizard.UI/Menu/MainMenuScreen.cs
@@ -0,0 +1,18 @@
+﻿using TutorLizard.UI.Menu.Screens;
+using TutorLizard.UI.Utilities;
+
+namespace TutorLizard.UI.Menu;
+public class MainMenuScreen : SimpleMenuScreenBase
+{
+    public MainMenuScreen(IMenuService menuService) : base(menuService)
+    {
+    }
+
+    public override MenuNavigation Display()
+    {
+        Console.WriteLine("Tutor Lizard - Korepetycje z Jaszczurem");
+        Console.ReadLine();
+
+        return MenuNavigation.QuitProgram;
+    }
+}
