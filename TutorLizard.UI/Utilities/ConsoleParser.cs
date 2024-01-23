17677359c228085c2f869f11608bae1c89cb21f9
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-23 12:06:34 +0100

    Add ConsoleParser and ParserOptions classes to UI.Utilities
    ConsoleParser is a static class that will have methods that should help request and parse user input. These methods should handle displaying messages to the user, retrying, converting input to specific data types and validation.
    ParserOptions defines an object that can be passed to the ConsoleParser methods to configure their behaviour.

---
+++ TutorLizard.UI/Utilities/ConsoleParser.cs
@@ -0,0 +1,38 @@
+﻿namespace TutorLizard.UI.Utilities;
+public static class ConsoleParser
+{
+    public static int? AskForInt(ParserOptions options)
+    {
+        string? input;
+        int output;
+        bool retry = false;
+
+        if (string.IsNullOrEmpty(options.StartMessage) == false)
+            Console.WriteLine(options.StartMessage);
+
+        do
+        {
+            if (retry && string.IsNullOrEmpty(options.RetryMessage) == false)
+                Console.WriteLine(options.RetryMessage);
+            
+            if (string.IsNullOrEmpty(options.Message) == false)
+                Console.Write(options.Message);
+
+            input = Console.ReadLine();
+
+            if (input is not null && input.Trim().ToLower() == options.ExitString)
+            {
+                return null;
+            }
+
+            bool parsed = int.TryParse(input, out output);
+
+            if (parsed == false || options.Predicate.Invoke(output) == false)
+                retry = true;
+            else
+                retry = false;
+        } while (retry);
+
+        return output;
+    }
+}
---
+++ TutorLizard.UI/Utilities/ParserOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace TutorLizard.UI.Utilities;
+
+public class ParserOptions
+{
+    public string? StartMessage { get; set; }
+    public string? Message { get; set; }
+    public string? RetryMessage { get; set; }
+    public string ExitString { get; set; } = "exit";
+    public Predicate<int> Predicate { get; set; } = x => true;
+
+}
