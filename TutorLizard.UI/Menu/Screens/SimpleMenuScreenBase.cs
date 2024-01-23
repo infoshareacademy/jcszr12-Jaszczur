a6ba3a2a225cbd2b84479fab3bd06bda098cb918
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-23 11:42:53 +0100

    Add MenuScreen base classes
    MenuScreens should inherit one of these base classes. The difference between them is the services they have access to.

---
+++ TutorLizard.UI/Menu/Screens/SimpleMenuScreenBase.cs
@@ -0,0 +1,11 @@
+﻿namespace TutorLizard.UI.Menu.Screens;
+public abstract class SimpleMenuScreenBase : IMenuScreen
+{
+    protected readonly IMenuService _menuService;
+
+    public abstract MenuNavigation Display();
+    public SimpleMenuScreenBase(IMenuService menuService)
+    {
+        _menuService = menuService;
+    }
+}
---
+++ TutorLizard.UI/Menu/Screens/StudentMenuScreenBase.cs
@@ -0,0 +1,15 @@
+﻿using TutorLizard.BusinessLogic.Services;
+
+namespace TutorLizard.UI.Menu.Screens;
+public abstract class StudentMenuScreenBase : IMenuScreen
+{
+    protected readonly IMenuService _menuService;
+    protected readonly IStudentService _studentService;
+
+    public abstract MenuNavigation Display();
+    public StudentMenuScreenBase(IMenuService menuService, IStudentService studentService)
+    {
+        _menuService = menuService;
+        _studentService = studentService;
+    }
+}
