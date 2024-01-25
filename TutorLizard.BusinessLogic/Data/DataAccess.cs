d0f91116bcc76cfe4c9ab6021ebe38b35f0ef994
Grzegorz Kmieciński <kmiecinskigrzegorz@gmail.com>
2024-01-25 17:03:08 +0100

    Add Saving data functionality
    Add generic method JsonDataAdder in DataAccess class; Add constructor and GetID static class for User class; Add User.json file in Data folder.

--- TutorLizard.BusinessLogic/Data/DataAccess.cs
+++ TutorLizard.BusinessLogic/Data/DataAccess.cs
@@ -1,5 +1,21 @@
-﻿namespace TutorLizard.BusinessLogic.Data;
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace TutorLizard.BusinessLogic.Data;
 public class DataAccess : IUserIdentityDataAccess, IStudentDataAccess, ITutorDataAccess
 {
     // This class will handle loading and saving data.
+
+    public static void JsonDataAdder<T>(T objectData)
+    {
+        var filePath = $@"{Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName}\{typeof(T).Name}.json";
+
+        var jsonData = System.IO.File.ReadAllText(filePath);
+        var objectList = JsonConvert.DeserializeObject<List<T>>(jsonData)
+                ?? new List<T>();
+
+        objectList.Add(objectData);
+        jsonData = JsonConvert.SerializeObject(objectList);
+        System.IO.File.WriteAllText(filePath, jsonData);
+    }
 }
--- TutorLizard.BusinessLogic/Models/User.cs
+++ TutorLizard.BusinessLogic/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +13,25 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; }
     public UserType UserType { get; set; }
+
+    public User(int id, string name, UserType type)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.UserType = type;
+    }
+    public static int GetID()
+    {
+        var filePath = $@"{Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName}\User.json";
+
+        var jsonData = System.IO.File.ReadAllText(filePath);
+        var userList = JsonConvert.DeserializeObject<List<User>>(jsonData)
+                ?? new List<User>();
+
+        if (userList.Count > 0)
+            return userList.Max(x => x.Id) + 1;
+        else
+            return 0;
+    }
 }
-public enum UserType { Tutor, Student }
+public enum UserType { Tutor, Student } 
