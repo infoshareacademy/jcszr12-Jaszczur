e5665aea0e4662890d5c0dd44d34aea9303b85c5
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-05 12:18:19 +0100

    Add methods needed by UI to IUserIdentityService interface
    Added methods that UI will need from IUserIdentityService . They were also added to UserIdentityService (not implemented).

--- TutorLizard.BusinessLogic/Services/UserIdentityService.cs
+++ TutorLizard.BusinessLogic/Services/UserIdentityService.cs
@@ -10,9 +10,42 @@ public class UserIdentityService : IUserIdentityService
     {
         _dataAccess = dataAccess;
     }
-
     public UserType? GetUserType()
     {
+        // return UserType of active user
+        // return null if not logged in
+        throw new NotImplementedException();
+    }
+    public string? GetUserName()
+    {
+        // return Name of active user
+        // return null if not logged in
+        throw new NotImplementedException();
+    }
+    public int? GetUserId()
+    {
+        // return Id of active user
+        // return null if not logged in
+        throw new NotImplementedException();
+    }
+    public bool IsUserNameTaken(string userName)
+    {
+        // return true if user with this userName already exists
+        throw new NotImplementedException();
+    }
+    public bool LogIn(string userName, int userId)
+    {
+        // return true if successful
+        throw new NotImplementedException();
+    }
+    public void LogOut()
+    {
+        throw new NotImplementedException();
+    }
+    public int RegisterUser(string userName)
+    {
+        // return id of newly created user
+        // return 0 if unsuccessful
         throw new NotImplementedException();
     }
 }
