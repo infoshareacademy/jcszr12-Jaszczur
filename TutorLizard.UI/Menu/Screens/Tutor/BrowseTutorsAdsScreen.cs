9545fdc9a51ca99629363170fcaf55f72ede10a1
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-06 11:59:11 +0100

    Modify BrowseTutorsAdsScreen to use Paginator utility

--- TutorLizard.UI/Menu/Screens/Tutor/BrowseTutorsAdsScreen.cs
+++ TutorLizard.UI/Menu/Screens/Tutor/BrowseTutorsAdsScreen.cs
@@ -5,41 +5,24 @@ using TutorLizard.UI.Utilities;
 namespace TutorLizard.UI.Menu.Screens.Tutor;
 public class BrowseTutorsAdsScreen : TutorMenuScreenBase
 {
-    private int _page = 0;
-    private int _adsPerPage = 4;
+    private int _page = 1;
+    Paginator<Ad> _paginator;
     public BrowseTutorsAdsScreen(IMenuService menuService, ITutorService tutorService) : base(menuService, tutorService)
     {
+        List<Ad> ads = _tutorService.GetUsersAds();
+        PaginatorOptions<Ad> options = new()
+        {
+            CollectionName = "Ogłoszenia",
+            DisplayItem = DisplayAd
+        };
+        _paginator = new(ads, options);
     }
 
     public override MenuNavigation Display()
     {
-        List<Ad> ads = _tutorService.GetUsersAds();
-
-        int pagesTotal = ads.Count / _adsPerPage;
-        if (ads.Count % _adsPerPage != 0)
-            pagesTotal++;
-
-        int firstItem = _page * _adsPerPage;
-        int lastItem = Math.Min((_page + 1) * _adsPerPage, ads.Count);
-
         Console.WriteLine("Twoje ogłoszenia");
         Console.WriteLine();
-        Console.WriteLine($"Strona {_page + 1} z {pagesTotal}\tOgłoszenia {firstItem + 1}-{lastItem} z {ads.Count}");
-        Console.WriteLine();
-
-        for (int i = firstItem; i < lastItem; i++)
-        {
-            DisplayAd(ads[i], i);
-        }
-
-        if (pagesTotal == 1)
-            return OnlyPageOptions();
-        else if (_page == 0)
-            return FirstPageOptions();
-        else if (_page + 1 < pagesTotal)
-            return MiddlePageOptions();
-        else
-            return LastPageOptions();
+        return _paginator.DisplayPage(ref _page);
     }
 
     private void DisplayAd(Ad ad, int i)
@@ -49,50 +32,5 @@ public class BrowseTutorsAdsScreen : TutorMenuScreenBase
         Console.WriteLine($"Temat: {ad.Subject}");
         Console.WriteLine($"Opis: {ad.Description}");
         Console.WriteLine();
-    }
-
-    private MenuNavigation FirstPageOptions()
-    {
-        int selected = SelectTool.SelectOne(["Kolejna strona", "Wróc do menu głównego"]);
-        switch (selected)
-        {
-            case 0:
-                _page++;
-                return MenuNavigation.NextOrCurrent;
-            default:
-                return MenuNavigation.Previous;
-        }
-    }
-    private MenuNavigation MiddlePageOptions()
-    {
-        int selected = SelectTool.SelectOne(["Kolejna strona", "Poprzednia strona", "Wróc do menu głównego"]);
-        switch (selected)
-        {
-            case 0:
-                _page++;
-                return MenuNavigation.NextOrCurrent;
-            case 1:
-                _page--;
-                return MenuNavigation.NextOrCurrent;
-            default:
-                return MenuNavigation.Previous;
-        }
-    }
-    private MenuNavigation LastPageOptions()
-    {
-        int selected = SelectTool.SelectOne(["Poprzednia strona", "Wróc do menu głównego"]);
-        switch (selected)
-        {
-            case 0:
-                _page--;
-                return MenuNavigation.NextOrCurrent;
-            default:
-                return MenuNavigation.Previous;
-        }
-    }
-    private MenuNavigation OnlyPageOptions()
-    {
-        int selected = SelectTool.SelectOne(["Wróc do menu głównego"]);
-        return MenuNavigation.Previous;
     }
 }
