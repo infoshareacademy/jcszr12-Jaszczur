f82cd936dcee8a2e60bf3f7c340c3683743f7271
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-07 14:01:11 +0100

    Add LoginScreen and RegisterUserScreen
    Not implemented.

---
+++ TutorLizard.UI/Menu/Screens/UserIdentity/LoginScreen.cs
@@ -0,0 +1,14 @@
+﻿using TutorLizard.BusinessLogic.Services;
+
+namespace TutorLizard.UI.Menu.Screens.UserIdentity;
+public class LoginScreen : UserIdentityMenuScreenBase
+{
+    public LoginScreen(IMenuService menuService, IUserIdentityService userIdentityService) : base(menuService, userIdentityService)
+    {
+    }
+
+    public override MenuNavigation Display()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TutorLizard.UI/Menu/Screens/UserIdentity/RegisterUserScreen.cs
@@ -0,0 +1,14 @@
+﻿using TutorLizard.BusinessLogic.Services;
+
+namespace TutorLizard.UI.Menu.Screens.UserIdentity;
+public class RegisterUserScreen : UserIdentityMenuScreenBase
+{
+    public RegisterUserScreen(IMenuService menuService, IUserIdentityService userIdentityService) : base(menuService, userIdentityService)
+    {
+    }
+
+    public override MenuNavigation Display()
+    {
+        throw new NotImplementedException();
+    }
+}
