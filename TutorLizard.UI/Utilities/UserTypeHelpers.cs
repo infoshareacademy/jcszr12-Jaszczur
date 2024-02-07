f8d2dbe3dd3e5ccf7e06d5801bc6da2087bb01c2
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-07 15:12:52 +0100

    Add UserTypeHelpers.cs
    It contains UiName extension method that returns UI-friendly name of each UserType.

---
+++ TutorLizard.UI/Utilities/UserTypeHelpers.cs
@@ -0,0 +1,19 @@
+﻿using TutorLizard.BusinessLogic.Models;
+
+namespace TutorLizard.UI.Utilities;
+public static class UserTypeHelpers
+{
+    public static string UiName(this UserType? userType)
+    {
+        switch (userType)
+        {
+            case UserType.Tutor:
+                return "Nauczyciel";
+            case UserType.Student:
+                return "Uczeń";
+            case null:
+            default:
+                return "";
+        }
+    }
+}
