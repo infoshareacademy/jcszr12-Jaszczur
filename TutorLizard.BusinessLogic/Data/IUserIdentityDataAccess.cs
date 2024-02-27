c28be62d9c6452e066eda66d99f84d4b1445321b
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-02-27 10:58:06 +0100

    Fix inconsistent nullability in IsLoginDataCorrect method
    Signature in interface didn't have nullable User return type, but the method in DataAccess implementing it did.

--- TutorLizard.BusinessLogic/Data/IUserIdentityDataAccess.cs
+++ TutorLizard.BusinessLogic/Data/IUserIdentityDataAccess.cs
@@ -5,7 +5,7 @@ namespace TutorLizard.BusinessLogic.Data;
 public interface IUserIdentityDataAccess
 {
     public bool DoesUserWithThisNameExist(string username);
-    public (bool isCorrect, User activeUser) IsLoginDataCorrect(int id, string username);
+    public (bool isCorrect, User? activeUser) IsLoginDataCorrect(int id, string username);
     public User CreateUser(string name, UserType type);
     public string GetUserNameById(int userId);
 }
