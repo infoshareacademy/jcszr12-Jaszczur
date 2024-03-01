8403f9f474c78522dd4d5db994a53fe667eec966
skrawus <wudarskap@gmail.com>
2024-03-01 10:29:06 +0100

    Create Schedule item request controller

--- TutorLizard.Web/Controllers/ScheduleItemRequestController.cs
+++ TutorLizard.Web/Controllers/ScheduleItemRequestController.cs
@@ -8,25 +8,22 @@ namespace TutorLizard.Web.Controllers
     public class ScheduleItemRequestController : Controller
     {
         private readonly DataAccess _dataAccess = new();
-        private static List<ScheduleItemRequest> scheduleItemRequests = new List<ScheduleItemRequest>();
-
-        public ScheduleItemRequestController() 
-        {
-            _dataAccess = new DataAccess();
-        }
 
         // GET: ScheduleItemRequestController
         public ActionResult Index()
         {
-            var model = _dataAccess.GetAllScheduleItemRequests();
-
-            return View(model);
+            return View(_dataAccess.GetAllScheduleItemRequests());
         }
 
         // GET: ScheduleItemRequestController/Details/5
         public ActionResult Details(int id)
         {
             var model = _dataAccess.GetScheduleItemRequestById(id);
+
+            if (model == null)
+            {
+                return RedirectToAction("Index");
+            }
 
             return View(model);
         }
@@ -42,24 +39,48 @@ namespace TutorLizard.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(ScheduleItemRequest model)
         {
-            scheduleItemRequests.Add(model);
-            TempData["Sukces!"] = "Zapytanie o zajęcia zostało wysłane";
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                int scheduleItemId = model.ScheduleItemId;
+                int studentId = model.StudentId;
+                bool isAccepted = model.IsAccepted;
+
+                _dataAccess.CreateScheduleItemRequest(scheduleItemId, studentId, isAccepted);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: ScheduleItemRequestController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var model = _dataAccess.GetScheduleItemRequestById(id);
+
+            if (model == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
 
         // POST: ScheduleItemRequestController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, ScheduleItemRequest model)
         {
             try
             {
+                _dataAccess.UpdateScheduleItemRequest(model);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -71,21 +92,30 @@ namespace TutorLizard.Web.Controllers
         // GET: ScheduleItemRequestController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var model = _dataAccess.GetScheduleItemRequestById(id);
+
+            if (model == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
 
         // POST: ScheduleItemRequestController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, ScheduleItemRequest model)
         {
             try
             {
+                _dataAccess.DeleteScheduleItemById(id);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }
