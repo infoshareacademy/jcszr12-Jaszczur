a9112e03fa2b6ef047c9030fc401b7378b71bd4c
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-03-01 10:29:37 +0100

    Add spacing to User.cs for readibility

--- TutorLizard.BusinessLogic/Models/User.cs
+++ TutorLizard.BusinessLogic/Models/User.cs
@@ -5,22 +5,27 @@ namespace TutorLizard.BusinessLogic.Models;
 public class User
 {
     public int Id { get; set; }
+
     [Required(ErrorMessage = "Podaj nazwę użytkownika.")]
     [Display(Name = "Nazwa użytkownika")]
     [MinLength(5, ErrorMessage = "Minimalna długość: 5 znaków")]
     [MaxLength(20, ErrorMessage = "Maksymalna długość: 20 znaków")]
     public string Name { get; set; }
+
     [Display(Name="Typ użytkownika")]
     public UserType UserType { get; set; }
+
     [Required(ErrorMessage = "Podaj adres email.")]
     [EmailAddress(ErrorMessage = "Podaj prawidłowy adres email.")]
     [Display(Name = "Adres email")]
     public string Email { get; set; }
+
     [Required(ErrorMessage = "Podaj hasło.")]
     [DataType(DataType.Password)]
     [Display(Name = "Hasło")]
     [MinLength(5, ErrorMessage = "Minimalna długość: 5 znaków")]
     public string PasswordHash { get; set; }
+
     [Display(Name = "Data rejestracji")]
     public DateTime DateCreated { get; set; } = DateTime.Now;
 
