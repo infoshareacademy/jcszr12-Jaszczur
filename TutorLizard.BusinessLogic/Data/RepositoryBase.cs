567bb559a07cdae1057e1edc2284cd5504de5e53
skrawus <wudarskap@gmail.com>
2024-03-12 10:33:14 +0100

    Add RepositoryBase

---
+++ TutorLizard.BusinessLogic/Data/RepositoryBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using TutorLizard.BusinessLogic.Models;
+using System.Diagnostics;
+
+namespace TutorLizard.BusinessLogic.Data
+{
+    public abstract class RepositoryBase<T>
+    {
+        protected List<T> _data = new();
+
+        private string _filePath = @"Data/users.json";
+
+        public RepositoryBase(string filePath)
+        {
+            _filePath = filePath;
+            LoadFromJson();
+        }
+
+
+        protected void LoadFromJson()
+        {
+            string filePath = Path.Combine(_filePath.Split('/'));
+            string fullPath;
+            if (Path.IsPathRooted(filePath))
+                fullPath = filePath;
+            else
+                fullPath = Path.Combine(AppContext.BaseDirectory, filePath);
+
+            if (!File.Exists(fullPath))
+                _data = new List<T>();
+
+            var jsonData = File.ReadAllText(fullPath);
+
+            var outputList = JsonSerializer.Deserialize<List<T>>(jsonData, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                IncludeFields = true
+            })
+                        ?? new List<T>();
+            _data = outputList;
+        }
+
+
+        protected void SaveToJson()
+        {
+            string filePath = Path.Combine(_filePath.Split('/'));
+            string fullPath;
+            if (Path.IsPathRooted(filePath))
+                fullPath = filePath;
+            else
+                fullPath = Path.Combine(AppContext.BaseDirectory, filePath);
+
+            string? directoryPath = Path.GetDirectoryName(fullPath);
+
+            var jsonData = JsonSerializer.Serialize(_data, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
+
+            if (directoryPath is not null && Directory.Exists(directoryPath) == false)
+                Directory.CreateDirectory(directoryPath);
+
+            File.WriteAllText(fullPath, jsonData);
+        }     
+    }
+}
