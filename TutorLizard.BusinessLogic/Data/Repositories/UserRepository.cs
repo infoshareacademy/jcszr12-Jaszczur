8f1e93800ab945baf5ebfb68aeb613cb671767f5
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-03-15 21:09:47 +0100

    Add UserRepository and IUserRepository

---
+++ TutorLizard.BusinessLogic/Data/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
+using TutorLizard.BusinessLogic.Models;
+
+namespace TutorLizard.BusinessLogic.Data.Repositories;
+public class UserRepository : RepositoryBase<User>, IUserRepository
+{
+    protected override string FilePath => "Data/users.json";
+    public UserRepository()
+    {
+        LoadFromJson();
+    }
+
+    public User CreateUser(string name, UserType type, string email, string passwordHash)
+    {
+        User newUser = new User(GetNewId(), name, type, email, passwordHash);
+        _data.Add(newUser);
+        SaveToJson();
+
+        return newUser;
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return _data;
+    }
+
+    public User? GetUserById(int id)
+    {
+        return _data.Find(x => x.Id == id);
+    }
+
+    public void UpdateUser(User user)
+    {
+        var toUpdate = GetUserById(user.Id);
+        if (toUpdate is null)
+            return;
+
+        toUpdate.Name = user.Name;
+        toUpdate.UserType = user.UserType;
+        toUpdate.Email = user.Email;
+        toUpdate.PasswordHash = user.PasswordHash;
+
+        SaveToJson();
+    }
+
+    public void DeleteUserById(int id)
+    {
+        var toDelete = GetUserById(id);
+        if (toDelete is null)
+            return;
+        _data.Remove(toDelete);
+
+        SaveToJson();
+    }
+
+    private int GetNewId()
+    {
+        if (_data.Any())
+            return _data.Max(x => x.Id);
+
+        return 1;
+    }
+}
