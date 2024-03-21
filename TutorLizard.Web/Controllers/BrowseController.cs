f937555931dcb7127a699a038f7991c991c5d03d
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-03-21 15:43:05 +0100

    Add actions to BrowseController, redirect Index to Ads

--- TutorLizard.Web/Controllers/BrowseController.cs
+++ TutorLizard.Web/Controllers/BrowseController.cs
@@ -12,6 +12,25 @@ public class BrowseController : Controller
     }
     public IActionResult Index()
     {
+        return RedirectToAction(nameof(Ads));
+    }
+
+    public IActionResult Ads()
+    {
+        // TODO ask service for ads to show
+        // TODO add pagination
+        return View();
+    }
+
+    public IActionResult AdDetails(int id)
+    {
+        // TODO ask service for ad to show based on id
+        return View();
+    }
+
+    public IActionResult Schedule()
+    {
+        // TODO get userId from HttpContext and ask service for ScheduleItems based on it
         return View();
     }
 }
