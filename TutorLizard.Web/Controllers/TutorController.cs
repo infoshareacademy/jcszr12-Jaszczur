34e9c15286c06c123e3d2404b1e8c99193965aaf
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-03-25 13:42:49 +0100

    Add empty TutorController

---
+++ TutorLizard.Web/Controllers/TutorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutorLizard.Web.Controllers;
+public class TutorController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
