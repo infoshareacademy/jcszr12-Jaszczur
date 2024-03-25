335516b3da30f30e1907f9841feef31602b1c63f
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-03-25 13:11:39 +0100

    Add ScheduleItemDto

---
+++ TutorLizard.BusinessLogic/Models/DTOs/ScheduleItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TutorLizard.BusinessLogic.Models.DTOs;
+public class ScheduleItemDto
+{
+    public int Id { get; set; }
+    public int AdId { get; set; }
+    public DateTime DateTime { get; set; }
+
+    public ScheduleItemDto(ScheduleItem scheduleItem)
+    {
+        Id = scheduleItem.Id;
+        AdId = scheduleItem.AdId;
+        DateTime = scheduleItem.DateTime;
+    }
+}
