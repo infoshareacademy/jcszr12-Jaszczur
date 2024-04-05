9e319142f5d23c7b9966412cdb78c8046f2208a6
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-05 08:52:41 +0200

    Empty out ITutorService interface

--- TutorLizard.BusinessLogic/Services/ITutorService.cs
+++ TutorLizard.BusinessLogic/Services/ITutorService.cs
@@ -4,24 +4,4 @@ namespace TutorLizard.BusinessLogic.Services;
 
 public interface ITutorService
 {
-    public Ad CreateAd(string subject,
-              string title,
-              string description,
-              int categoryId,
-              double price,
-              string location,
-              bool isRemote);
-    public ScheduleItem CreateScheduleItem(int adId, DateTime dateTime);
-    public Ad? GetAdById(int adId);
-    public ScheduleItem? GetScheduleItemById(int scheduleItemId);
-    public List<Ad> GetTutorsAds();
-    public List<ScheduleItem> GetTutorsScheduleItems();
-    public List<AdRequest> GetTutorsAdRequests();
-    public List<ScheduleItemRequest> GetTutorsScheduleItemRequests();
-    public string GetStudentUserNameByAdRequestId(int adRequestId);
-    public string GetStudentUserNameByScheduleItemRequestId(int scheduleItemRequestId);
-    public bool TutorCanEditAdSchedule(int adId);
-    public AdRequest AcceptAdRequest(int adRequestId); 
-    public ScheduleItemRequest AcceptScheduleItemRequest(int scheduleItemRequestId);
-    public bool IsScheduleItemFree(ScheduleItem scheduleItem);
 }
