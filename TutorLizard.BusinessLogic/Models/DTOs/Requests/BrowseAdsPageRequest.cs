a38db9b5ee4567a199376e2d3c44ca64c131270a
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-09 12:36:06 +0200

    Add BrowseAdsPage Request and Response DTOs, AdListItemDto
    Now Browse/Ads view and _AdListItem use their own DTOs as models.

---
+++ TutorLizard.BusinessLogic/Models/DTOs/Requests/BrowseAdsPageRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace TutorLizard.BusinessLogic.Models.DTOs.Requests;
+public class BrowseAdsPageRequest(int pageNumber, int pageSize)
+{
+    public int PageNumber { get; set; } = pageNumber;
+    public int PageSize { get; set; } = pageSize;
+}
--- TutorLizard.Web/Controllers/BrowseController.cs
+++ TutorLizard.Web/Controllers/BrowseController.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TutorLizard.BusinessLogic.Extensions;
 using TutorLizard.BusinessLogic.Interfaces.Services;
-using TutorLizard.BusinessLogic.Models;
-using TutorLizard.BusinessLogic.Models.DTOs;
+using TutorLizard.BusinessLogic.Models.DTOs.Requests;
+using TutorLizard.BusinessLogic.Models.DTOs.Responses;
 
 namespace TutorLizard.Web.Controllers;
 public class BrowseController : Controller
 {
     private readonly IBrowseService _browseService;
+    private readonly int _pageSize;
 
     public BrowseController(IBrowseService browseService)
     {
         _browseService = browseService;
+        _pageSize = 10;
     }
     public IActionResult Index()
     {
@@ -20,18 +21,142 @@ public class BrowseController : Controller
 
     public IActionResult Ads(int page = 1)
     {
-        // TODO ask service for ads to show
         // TODO add pagination
 
-        List<AdDto> ads = [
-            new Ad(1, 1, "temat", "tytuł", "opis", 1, 10, "Warszawa", true).ToDto(),
-            new Ad(2, 1, "temat", "tytuł", "opis", 2, 10, "Kraków", true).ToDto(),
-            new Ad(3, 1, "temat", "tytuł", "opis", 1, 10, "Gdańsk", true).ToDto(),
-            new Ad(4, 1, "temat", "tytuł", "opis", 2, 10, "Kosmos", true).ToDto(),
-            new Ad(5, 1, "temat", "tytuł", "opis", 1, 10, "Wieś", true).ToDto(),
+        BrowseAdsPageRequest request = new(page, _pageSize);
+
+        // TODO ask service for ads to show (using request)
+
+        // mock results:
+        BrowseAdsPageResponse response = new();
+        response.PageNumber = page;
+        response.PageSize = _pageSize;
+        response.TotalPages = 5;
+
+        response.Ads = [
+                new(
+                    id: 1,
+                    tutorId: 1,
+                    tutorName: "Nauczyciel 1",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 1,
+                    categoryName: "Matematyka",
+                    price: 100m,
+                    location: "Warszawa",
+                    isRemote: true),
+                new(
+                    id: 2,
+                    tutorId: 2,
+                    tutorName: "Nauczyciel 2",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 2,
+                    categoryName: "Język polski",
+                    price: 150m,
+                    location: "Kraków",
+                    isRemote: false),
+                new(
+                    id: 3,
+                    tutorId: 1,
+                    tutorName: "Nauczyciel 1",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 3,
+                    categoryName: "Język angielski",
+                    price: 1000m,
+                    location: "Kosmos",
+                    isRemote: true),
+                new(
+                    id: 4,
+                    tutorId: 3,
+                    tutorName: "Nauczyciel 3",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 4,
+                    categoryName: "Geografia",
+                    price: 10.50m,
+                    location: "Papua Nowa Gwinea",
+                    isRemote: true),
+                new(
+                    id: 5,
+                    tutorId: 4,
+                    tutorName: "Nauczyciel 4",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 1,
+                    categoryName: "Matematyka",
+                    price: 500m,
+                    location: "Gdańsk",
+                    isRemote: false),
+                new(
+                    id: 6,
+                    tutorId: 5,
+                    tutorName: "Nauczyciel 5",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 1,
+                    categoryName: "Matematyka",
+                    price: 499.99m,
+                    location: "Gdańsk",
+                    isRemote: false),
+                new(
+                    id: 7,
+                    tutorId: 5,
+                    tutorName: "Nauczyciel 5",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 2,
+                    categoryName: "Język polski",
+                    price: 99.99m,
+                    location: "Gdańsk",
+                    isRemote: true),
+                new(
+                    id: 8,
+                    tutorId: 1,
+                    tutorName: "Nauczyciel 1",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 5,
+                    categoryName: "WF",
+                    price: 159.73m,
+                    location: "Warszawa",
+                    isRemote: true),
+                new(
+                    id: 9,
+                    tutorId: 2,
+                    tutorName: "Nauczyciel 2",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 3,
+                    categoryName: "Język angielski",
+                    price: 10m,
+                    location: "Gniezno",
+                    isRemote: false),
+                new(
+                    id: 10,
+                    tutorId: 19,
+                    tutorName: "Nauczyciel 19",
+                    subject: "tematyka",
+                    title: "tytuł",
+                    description: "opis",
+                    categoryId: 19,
+                    categoryName: "Metafizyka",
+                    price: .99m,
+                    location: "Berlin",
+                    isRemote: false),
             ];
 
-        return View(ads);
+        return View(response);
     }
 
     public IActionResult AdDetails(int id)
