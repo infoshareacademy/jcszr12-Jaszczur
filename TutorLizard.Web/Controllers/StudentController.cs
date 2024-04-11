c8a55c8d90db09457da372da9e850d5e61643e39
skrawus <wudarskap@gmail.com>
2024-04-12 00:49:05 +0200

    Add StudentAcceptedAdsView

--- TutorLizard.Web/Controllers/StudentController.cs
+++ TutorLizard.Web/Controllers/StudentController.cs
@@ -1,17 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TutorLizard.BusinessLogic.Interfaces.Services;
+using TutorLizard.BusinessLogic.Models.DTOs;
+using TutorLizard.BusinessLogic.Models.DTOs.Requests;
+using TutorLizard.BusinessLogic.Models.DTOs.Responses;
 
 namespace TutorLizard.Web.Controllers;
+[Authorize]
 public class StudentController : Controller
 {
     private readonly IStudentService _studentService;
+    private readonly IUserAuthenticationService _userAuthenticationService;
 
-    public StudentController(IStudentService studentService)
+    public StudentController(IStudentService studentService,
+                             IUserAuthenticationService userAuthenticationService)
     {
         _studentService = studentService;
+        _userAuthenticationService = userAuthenticationService;
     }
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult AcceptedAds()
+    {
+        int? studentId = _userAuthenticationService.GetLoggedInUserId();
+        StudentsAcceptedAdsRequest request = new(studentId);
+
+        try
+        {
+            if (ModelState.IsValid == false)
+                return View(request);
+            
+            if(studentId is null)
+            {
+                return View(request);
+            }
+            request.StudentId = (int)studentId;
+
+            StudentsAcceptedAdsResponse response = new()
+            {
+                // The data is only for tests
+                Ads = [new AdListItemDto(1, 1, "Jan", "Maths", "Matematyka", "opis", 1, "Math", 60, "Warszawa", true)]
+            };
+        }
+
+        catch
+        {
+            return View(request);
+        }
+        
+        return RedirectToAction(nameof(Index));
     }
 }
