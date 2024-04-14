5bfa6b12486fed983eceb10d62ce030f11404c9a
skrawus <wudarskap@gmail.com>
2024-04-14 22:45:25 +0200

    Fill view and fix StudentController test data

--- TutorLizard.Web/Controllers/StudentController.cs
+++ TutorLizard.Web/Controllers/StudentController.cs
@@ -66,7 +66,7 @@ public class StudentController : Controller
             StudentsAdRequestsResponse response = new()
             {
                 //data for tests only
-                AdRequests = [new AdRequestsListDto(1, 1, 1, true, "xyz", "yxz", false)]
+                AdRequests = [new AdRequestsListDto(1, 1, 1, false, "xyz", "yxz", false)]
             };
             return View(response);
         }
