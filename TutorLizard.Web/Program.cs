9b897a9f98272c15829603fa3c51adbf102e88a7
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-21 14:41:14 +0200

    Register DbRepositories for all models using LizardContext

--- TutorLizard.Web/Program.cs
+++ TutorLizard.Web/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using TutorLizard.BusinessLogic.Data.Repositories.DataBase;
 using TutorLizard.BusinessLogic.Data.Repositories.Json;
 using TutorLizard.BusinessLogic.Interfaces.Data;
 using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
 using TutorLizard.BusinessLogic.Interfaces.Repositories;
 using TutorLizard.BusinessLogic.Interfaces.Services;
+using TutorLizard.BusinessLogic.Models;
 using TutorLizard.BusinessLogic.Options;
 using TutorLizard.BusinessLogic.Services;
 
@@ -34,6 +36,13 @@ builder.Services.AddDbContext<LizardContext>(configuration =>
     configuration
         .UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
+
+builder.Services.AddScoped<IDbRepository<Ad>, DbRepository<Ad, LizardContext>>();
+builder.Services.AddScoped<IDbRepository<AdRequest>, DbRepository<AdRequest, LizardContext>>();
+builder.Services.AddScoped<IDbRepository<Category>, DbRepository<Category, LizardContext>>();
+builder.Services.AddScoped<IDbRepository<ScheduleItem>, DbRepository<ScheduleItem, LizardContext>>();
+builder.Services.AddScoped<IDbRepository<ScheduleItemRequest>, DbRepository<ScheduleItemRequest, LizardContext>>();
+builder.Services.AddScoped<IDbRepository<User>, DbRepository<User, LizardContext>>();
 
 builder.Services.AddAuthentication("CookieAuth")
     .AddCookie("CookieAuth",options =>
