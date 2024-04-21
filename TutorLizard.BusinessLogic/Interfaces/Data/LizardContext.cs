259e89bf38503b6f121826818d38b2799353c6ae
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-21 14:23:03 +0200

    Define table relationships in LizardContext.OnModelCreating()

--- TutorLizard.BusinessLogic/Interfaces/Data/LizardContext.cs
+++ TutorLizard.BusinessLogic/Interfaces/Data/LizardContext.cs
@@ -15,6 +15,46 @@ public class LizardContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<User>()
+            .HasMany(user => user.Ads)
+            .WithOne(ad => ad.Tutor)
+            .HasForeignKey(ad => ad.TutorId)
+            .IsRequired();
 
+        modelBuilder.Entity<User>()
+            .HasMany(user => user.ScheduleItemRequests)
+            .WithOne(request => request.Student)
+            .HasForeignKey(request => request.StudentId)
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .HasMany(user => user.AdRequests)
+            .WithOne(request => request.Student)
+            .HasForeignKey(request => request.StudentId)
+            .IsRequired();
+
+        modelBuilder.Entity<Ad>()
+            .HasOne(ad => ad.Category)
+            .WithMany(category  => category.Ads)
+            .HasForeignKey(ad => ad.CategoryId)
+            .IsRequired();
+
+        modelBuilder.Entity<Ad>()
+            .HasMany(ad => ad.ScheduleItems)
+            .WithOne(item => item.Ad)
+            .HasForeignKey(item => item.AdId)
+            .IsRequired();
+
+        modelBuilder.Entity<Ad>()
+            .HasMany(ad => ad.Requests)
+            .WithOne(request => request.Ad)
+            .HasForeignKey(request => request.AdId)
+            .IsRequired();
+
+        modelBuilder.Entity<ScheduleItem>()
+            .HasMany(item => item.Requests)
+            .WithOne(request => request.ScheduleItem)
+            .HasForeignKey(request => request.ScheduleItemId)
+            .IsRequired();
     }
 }
