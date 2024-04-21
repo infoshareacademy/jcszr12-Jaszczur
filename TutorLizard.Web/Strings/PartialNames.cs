644bc5e2dd96247b8a7e6cea6db7ec33a09a1d5a
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-21 11:59:49 +0200

    Add PartialNames static class, use it to set names property in partial tags

---
+++ TutorLizard.Web/Strings/PartialNames.cs
@@ -0,0 +1,8 @@
+﻿namespace TutorLizard.Web.Strings;
+
+public static class PartialNames
+{
+    public const string Notifications = "_Notifications";
+    public const string AdListItem = "_AdListItem";
+    public const string AdRequest = "_AdRequests";
+}
