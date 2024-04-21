f78a5d5a5b9ec11d73eab3968dbb20259a995773
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-21 14:12:59 +0200

    Add LizardContext - our DbContext

---
+++ TutorLizard.BusinessLogic/Interfaces/Data/LizardContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorLizard.BusinessLogic.Models;
+
+namespace TutorLizard.BusinessLogic.Interfaces.Data;
+public class LizardContext : DbContext
+{
+    DbSet<Ad> Ads { get; set; }
+    DbSet<AdRequest> AdRequests { get; set; }
+    DbSet<Category> Categories { get; set; }
+    DbSet<ScheduleItem> ScheduleItems { get; set; }
+    DbSet<ScheduleItemRequest> ScheduleItemRequests { get; set; }
+    DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+    }
+}
