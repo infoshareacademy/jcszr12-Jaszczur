a006fba4d3ed80b618b0103f883ff4aff4640b94
Grzegorz Kmieciński <kmiecinskigrzegorz@gmail.com>
2024-04-24 15:22:03 +0200

    remove unnecessary strings

--- TutorLizard.BusinessLogic/Models/DTOs/AdRequestsListDto.cs
+++ TutorLizard.BusinessLogic/Models/DTOs/AdRequestsListDto.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TutorLizard.BusinessLogic.Interfaces.Repositories;
-
-namespace TutorLizard.BusinessLogic.Models.DTOs;
+﻿namespace TutorLizard.BusinessLogic.Models.DTOs;
 
 public class AdRequestsListDto(int id,
                                int studentId,
