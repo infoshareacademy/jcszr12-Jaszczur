2661f4a42f49d6894c1637d3cb73fb3eb2ec2b78
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-25 13:39:40 +0200

    Add TutorsScheduleForAd component, DTOs

---
+++ TutorLizard.BusinessLogic/Models/DTOs/TutorsScheduleItemRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TutorLizard.BusinessLogic.Models.DTOs;
+public class TutorsScheduleItemRequestDto
+{
+    public int Id { get; set; }
+    public string StudentName { get; set; }
+    public int StudentId { get; set; }
+    public bool IsAccepted { get; set; }
+    public bool IsRemote { get; set; }
+    public DateTime DateCreated { get; set; }
+    public bool CanBeAccepted { get; set; } = true;
+}
