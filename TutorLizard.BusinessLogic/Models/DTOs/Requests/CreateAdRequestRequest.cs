ced95a01e9d610b956df68352cf1900159190a5c
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-04-25 15:04:10 +0200

    Add CreateRequestForAd component

---
+++ TutorLizard.BusinessLogic/Models/DTOs/Requests/CreateAdRequestRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TutorLizard.BusinessLogic.Models.DTOs.Requests;
+public class CreateAdRequestRequest
+{
+    public int StudentId { get; set; }
+
+    [Required]
+    public int AdId { get; set; }
+
+    [DisplayName("Wiadomość dla nauczyciela")]
+    [Required(ErrorMessage = "To pole jest wymagane")]
+    [MaxLength(500, ErrorMessage = "Maksymalna długość: 500 znaków")]
+    public string Message { get; set; }
+
+    [DisplayName("Nauczanie zdalne")]
+    [Required(ErrorMessage = "To pole jest wymagane")]
+    public bool IsRemote { get; set; }
+}
