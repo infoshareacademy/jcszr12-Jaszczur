3f66221d71daf65bf4b811515e61561ec2584b09
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-18 20:21:23 +0200

    Add BrowseControllerTests with setup and 1 test
    Test: Ads_WhenProvidedInvalidPageNumber_ShouldAskForPageOne

---
+++ Tests/TutorLizard.Web.Tests/Controllers/BrowseControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using Moq;
+using TutorLizard.BusinessLogic.Interfaces.Services;
+using TutorLizard.BusinessLogic.Models.DTOs.Requests;
+using TutorLizard.BusinessLogic.Models.DTOs.Responses;
+using TutorLizard.Web.Controllers;
+
+namespace TutorLizard.Web.Tests.Controllers;
+public class BrowseControllerTests
+{
+    private readonly BrowseController _browseController;
+    private readonly Mock<IBrowseService> _mockBrowseService = new();
+    private readonly Mock<IUserAuthenticationService> _mockUserAuthenticationService = new();
+    private readonly Fixture _fixture = new();
+    public BrowseControllerTests()
+    {
+        _browseController = new(_mockBrowseService.Object, _mockUserAuthenticationService.Object);
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(-1, 1)]
+    [InlineData(int.MinValue, 1)]
+    public async Task Ads_WhenProvidedInvalidPageNumber_ShouldAskForPageOne(int pageNumber, int expectedUsedPageNumber)
+    {
+        // Arrange
+        List<GetBrowseAdsPageRequest> requests = [];
+        GetBrowseAdsPageResponse response = _fixture
+            .Build<GetBrowseAdsPageResponse>()
+                .With(r => r.Success, true)
+            .Create();
+
+        _mockBrowseService
+            .Setup(x => x.GetBrowseAdsPage(Capture.In(requests)))
+            .Returns(Task.FromResult(response));
+
+        // Act
+        await _browseController.Ads(pageNumber);
+
+        // Assert
+        Assert.Equal(expectedUsedPageNumber, requests.Single().PageNumber);
+    }
+}
