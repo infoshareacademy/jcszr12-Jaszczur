df0c4d4b072d1a76112873442212a850b78219d1
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-21 14:56:55 +0200

    Add helper methods and more setup to BrowseServiceTests

--- Tests/TutorLizard.BusinessLogic.Tests/Services/BrowseServiceTests.cs
+++ Tests/TutorLizard.BusinessLogic.Tests/Services/BrowseServiceTests.cs
@@ -1,19 +1,31 @@
-﻿using Moq;
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using TutorLizard.BusinessLogic.Data;
 using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
 using TutorLizard.BusinessLogic.Models;
+using TutorLizard.BusinessLogic.Models.DTOs;
 using TutorLizard.BusinessLogic.Models.DTOs.Requests;
 using TutorLizard.BusinessLogic.Models.DTOs.Responses;
 using TutorLizard.BusinessLogic.Services;
 
 namespace TutorLizard.BusinessLogic.Tests.Services;
-public class BrowseServiceTests
+public class BrowseServiceTests : IDisposable
 {
     private readonly BrowseService _browseService;
     private readonly Mock<IDbRepository<Ad>> _mockAdRepository = new();
     private readonly Mock<IDbRepository<ScheduleItem>> _mockScheduleItemRepository = new();
+    private readonly Fixture _fixture = new();
+    private readonly JaszczurContext _dbContext;
     public BrowseServiceTests()
     {
         _browseService = new(_mockAdRepository.Object, _mockScheduleItemRepository.Object);
+        _dbContext = SetupInMemoryDbContext();
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Dispose();
     }
 
     [Theory]
@@ -38,5 +50,57 @@ public class BrowseServiceTests
 
         // Assert
         Assert.Equivalent(expected, actual);
+    }
+    private List<Ad> CreateTestAds(int adCount)
+    {
+        User user = _fixture
+            .Build<User>()
+                .Without(user => user.Ads)
+                .Without(user => user.AdRequests)
+                .Without(user => user.ScheduleItemRequests)
+            .Create();
+
+        Category category = _fixture
+            .Build<Category>()
+                .Without(category => category.Ads)
+            .Create();
+
+        List<Ad> ads = _fixture
+            .Build<Ad>()
+                .Without(ad => ad.Id)
+                .Without(ad => ad.AdRequests)
+                .Without(ad => ad.ScheduleItems)
+                .With(ad => ad.User, user)
+                .With(ad => ad.Category, category)
+                .With(ad => ad.Price, Math.Abs(_fixture.Create<decimal>()))
+            .CreateMany(adCount)
+            .ToList();
+
+        return ads;
+    }
+
+    private void SetupMockGetAllAds(List<Ad> ads)
+    {
+        var adsInDb = AddAdsToInMemoryDb(ads);
+        _mockAdRepository
+            .Setup(x => x.GetAll())
+            .Returns(adsInDb);
+    }
+
+    private IQueryable<Ad> AddAdsToInMemoryDb(List<Ad> ads)
+    {
+        _dbContext.Ads.AddRange(ads);
+        _dbContext.SaveChanges();
+
+        return _dbContext.Ads
+            .AsQueryable();
+    }
+
+    private JaszczurContext SetupInMemoryDbContext()
+    {
+        DbContextOptionsBuilder<JaszczurContext> dbBuilder = new();
+        dbBuilder.UseInMemoryDatabase(databaseName: $"FakeDb{Guid.NewGuid()}");
+        JaszczurContext context = new(dbBuilder.Options);
+        return context;
     }
 }
