b3fa6d1355c5c6dc2918578f7def9d6a147340d0
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-21 12:16:52 +0200

    Add BrowseServiceTests with basic setup

---
+++ Tests/TutorLizard.BusinessLogic.Tests/Services/BrowseServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
+using TutorLizard.BusinessLogic.Models;
+using TutorLizard.BusinessLogic.Services;
+
+namespace TutorLizard.BusinessLogic.Tests.Services;
+public class BrowseServiceTests
+{
+    private readonly BrowseService _browseService;
+    private readonly Mock<IDbRepository<Ad>> _mockAdRepository = new();
+    private readonly Mock<IDbRepository<ScheduleItem>> _mockScheduleItemRepository = new();
+    public BrowseServiceTests()
+    {
+        _browseService = new(_mockAdRepository.Object, _mockScheduleItemRepository.Object);
+    }
+}
