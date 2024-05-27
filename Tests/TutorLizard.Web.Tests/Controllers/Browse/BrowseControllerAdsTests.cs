d1cf84ff3e569b8e37beb14dca0afc027851510d
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-27 10:56:55 +0200

    Extract base class for BrowseControllerTests, split tests by action

---
+++ Tests/TutorLizard.Web.Tests/Controllers/Browse/BrowseControllerAdsTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TutorLizard.BusinessLogic.Models.DTOs.Requests;
+using TutorLizard.BusinessLogic.Models.DTOs.Responses;
+
+namespace TutorLizard.Web.Tests.Controllers.Browse;
+public class BrowseControllerAdsTests : BrowseControllerTestsBase
+{
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(-1, 1)]
+    [InlineData(int.MinValue, 1)]
+    public async Task Ads_WhenProvidedInvalidPageNumber_ShouldAskForPageOne(int pageNumber, int expectedUsedPageNumber)
+    {
+        // Arrange
+        List<GetBrowseAdsPageRequest> requests = [];
+        GetBrowseAdsPageResponse response = CreateGetBrowseAdsPageResponse(success: true);
+
+        MockBrowseService
+            .Setup(x => x.GetBrowseAdsPage(Capture.In(requests)))
+            .Returns(Task.FromResult(response));
+
+        // Act
+        await BrowseController.Ads(pageNumber);
+
+        // Assert
+        Assert.Equal(expectedUsedPageNumber, requests.Single().PageNumber);
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(100, 100)]
+    [InlineData(int.MaxValue, int.MaxValue)]
+    public async Task Ads_WhenProvidedValidPageNumber_ShouldAskForCorrectPage(int pageNumber, int expectedUsedPageNumber)
+    {
+        // Arrange
+        List<GetBrowseAdsPageRequest> requests = [];
+        GetBrowseAdsPageResponse response = CreateGetBrowseAdsPageResponse(success: true);
+
+        MockBrowseService
+            .Setup(x => x.GetBrowseAdsPage(Capture.In(requests)))
+            .Returns(Task.FromResult(response));
+
+        // Act
+        await BrowseController.Ads(pageNumber);
+
+        // Assert
+        Assert.Equal(expectedUsedPageNumber, requests.Single().PageNumber);
+    }
+
+    [Fact]
+    public async Task Ads_WhenResponseIsUnsuccessful_ShouldReturnRedirectToAction()
+    {
+        // Arrange
+        int pageNumber = 1;
+        GetBrowseAdsPageResponse response = CreateGetBrowseAdsPageResponse(success: false);
+
+        MockBrowseService
+           .Setup(x => x.GetBrowseAdsPage(It.IsAny<GetBrowseAdsPageRequest>()))
+           .Returns(Task.FromResult(response));
+
+        // Act
+        var result = await BrowseController.Ads(pageNumber);
+
+        // Assert
+        Assert.IsType<RedirectToActionResult>(result);
+    }
+
+    [Fact]
+    public async Task Ads_WhenResponseIsSuccessful_ShouldReturnView()
+    {
+        // Arrange
+        int pageNumber = 1;
+        GetBrowseAdsPageResponse response = CreateGetBrowseAdsPageResponse(success: true);
+
+        MockBrowseService
+           .Setup(x => x.GetBrowseAdsPage(It.IsAny<GetBrowseAdsPageRequest>()))
+           .Returns(Task.FromResult(response));
+
+        // Act
+        var result = await BrowseController.Ads(pageNumber);
+
+        // Assert
+        Assert.IsType<ViewResult>(result);
+    }
+}
---
+++ Tests/TutorLizard.Web.Tests/Controllers/Browse/BrowseControllerTestsBase.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using Moq;
+using TutorLizard.BusinessLogic.Interfaces.Services;
+using TutorLizard.BusinessLogic.Models.DTOs.Responses;
+using TutorLizard.Web.Controllers;
+
+namespace TutorLizard.Web.Tests.Controllers.Browse;
+
+public abstract class BrowseControllerTestsBase
+{
+    protected BrowseController BrowseController;
+    protected Mock<IBrowseService> MockBrowseService = new();
+    protected Mock<IUserAuthenticationService> MockUserAuthenticationService = new();
+    protected Fixture Fixture = new();
+
+    public BrowseControllerTestsBase()
+    {
+        BrowseController = new(MockBrowseService.Object, MockUserAuthenticationService.Object);
+    }
+    protected GetBrowseAdsPageResponse CreateGetBrowseAdsPageResponse(bool success)
+    {
+        return Fixture
+                    .Build<GetBrowseAdsPageResponse>()
+                        .With(r => r.Success, success)
+                    .Create();
+    }
+
+    protected void SetupMockGetLoggedInUserId(int? userId)
+    {
+        MockUserAuthenticationService
+           .Setup(x => x.GetLoggedInUserId())
+           .Returns(userId);
+    }
+}
