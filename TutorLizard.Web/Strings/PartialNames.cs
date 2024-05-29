85849ae3d30c480844e8a88899c554c3d0fb4ddc
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-29 09:37:35 +0200

    Add remaining partials to PartialNames
    Except ValidationScriptsPartial

--- TutorLizard.Web/Strings/PartialNames.cs
+++ TutorLizard.Web/Strings/PartialNames.cs
@@ -5,4 +5,6 @@ public static class PartialNames
     public const string Notifications = "_Notifications";
     public const string AdListItem = "_AdListItem";
     public const string AdRequest = "_AdRequests";
+    public const string PendingAdRequestsListItem = "_PendingAdRequestsListItem";
+    public const string TutorAllAdRequestsListItem = "_TutorAllAdRequestsListItem";
 }
