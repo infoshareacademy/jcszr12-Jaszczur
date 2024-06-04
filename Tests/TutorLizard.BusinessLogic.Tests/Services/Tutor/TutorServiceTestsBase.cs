84429b98b770e3a993b43fa10c8c4547361e2ff6
Grzegorz <kmiecinskigrzegorz@gmail.com>
2024-06-04 17:59:32 +0200

    Add Mocks for models in TutorServiceTestsBase

--- Tests/TutorLizard.BusinessLogic.Tests/Services/Tutor/TutorServiceTestsBase.cs
+++ Tests/TutorLizard.BusinessLogic.Tests/Services/Tutor/TutorServiceTestsBase.cs
@@ -22,4 +22,73 @@ public class TutorServiceTestsBase : TestsWithInMemoryDbBase
                            MockAdRequestRepository.Object,
                            MockAdRepository.Object);
     }
+
+    protected void SetupMockGetAllScheduleItems(List<ScheduleItem> scheduleItems) 
+    {
+        var scheduleItemsInDb = AddEntitiesToInMemoryDb(scheduleItems);
+
+        MockScheduleItemRepository
+            .Setup(x => x.GetAll())
+            .Returns(scheduleItemsInDb);
+    }
+    protected void SetupMockGetScheduleItemById(ScheduleItem? scheduleItem) 
+    {
+        MockScheduleItemRepository
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .Returns(Task.FromResult(scheduleItem));
+    }
+    protected void SetupMockGetAllScheduleItemRequests(List<ScheduleItemRequest> scheduleItemRequests)
+    {
+        var scheduleItemRequestsInDb = AddEntitiesToInMemoryDb(scheduleItemRequests);
+
+        MockScheduleItemRequestRepository
+            .Setup(x => x.GetAll())
+            .Returns(scheduleItemRequestsInDb);
+    }
+    protected void SetupMockGetScheduleItemRequestById(ScheduleItemRequest? scheduleItemRequest) 
+    {
+        MockScheduleItemRequestRepository
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .Returns(Task.FromResult(scheduleItemRequest));
+    }
+    protected void SetupMockGetAllAds(List<Ad> ads)
+    {
+        var adsInDb = AddEntitiesToInMemoryDb(ads);
+
+        MockAdRepository
+            .Setup(x => x.GetAll())
+            .Returns(adsInDb);
+    }
+    protected void SetupMockGetAdById(Ad? ad) 
+    {
+        MockAdRepository
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .Returns(Task.FromResult(ad));
+    }
+    protected void SetupMockGetAllAdRequests(List<AdRequest> adRequests) 
+    {
+        var adRequestsInDb = AddEntitiesToInMemoryDb (adRequests);
+
+        MockAdRequestRepository
+            .Setup(x => x.GetAll())
+            .Returns(adRequestsInDb);
+    }
+    protected void SetupMockGetAdRequestById(AdRequest? adRequest) 
+    {
+        MockAdRequestRepository
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .Returns(Task.FromResult(adRequest));
+    }
+    protected IQueryable<TEntity> AddEntitiesToInMemoryDb<TEntity>(List<TEntity> entities)
+    where TEntity : class
+    {
+        DbContext
+            .Set<TEntity>()
+            .AddRange(entities);
+        DbContext.SaveChanges();
+
+        return DbContext
+            .Set<TEntity>()
+            .AsQueryable();
+    }
 }
