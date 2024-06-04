3c3deaf8f1034c8bf538ff062baeece88cac09cc
Grzegorz <kmiecinskigrzegorz@gmail.com>
2024-06-04 17:41:27 +0200

    Initial TutorServiceTestsBase setup

---
+++ Tests/TutorLizard.BusinessLogic.Tests/Services/Tutor/TutorServiceTestsBase.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+using Moq;
+using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
+using TutorLizard.BusinessLogic.Models;
+using TutorLizard.BusinessLogic.Services;
+
+namespace TutorLizard.BusinessLogic.Tests.Services.Tutor;
+
+public class TutorServiceTestsBase : TestsWithInMemoryDbBase
+{
+    protected TutorService TutorService;
+    protected Fixture Fixture = new();
+    protected Mock<IDbRepository<ScheduleItem>> MockScheduleItemRepository = new();
+    protected Mock<IDbRepository<ScheduleItemRequest>> MockScheduleItemRequestRepository = new();
+    protected Mock<IDbRepository<Ad>> MockAdRepository = new();
+    protected Mock<IDbRepository<AdRequest>> MockAdRequestRepository = new();
+
+    protected TutorServiceTestsBase() : base()
+    {
+        TutorService = new(MockScheduleItemRepository.Object,
+                           MockScheduleItemRequestRepository.Object,
+                           MockAdRequestRepository.Object,
+                           MockAdRepository.Object);
+    }
+}
