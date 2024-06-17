431af05f43ab14073372bbe309c91db61f023805
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-17 22:28:38 +0200

    Add extension methods for AdSearchCriteriaDto with tests
    The methods are to convert this DTO to and from a Base64 string.

---
+++ Tests/TutorLizard.Web.Tests/Extensions/AdSearchCriteriaDtoExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using TutorLizard.BusinessLogic.Models.DTOs;
+using TutorLizard.Web.Extensions;
+
+namespace TutorLizard.Web.Tests.Extensions;
+public class AdSearchCriteriaDtoExtensionsTests
+{
+    Fixture _fixture = new();
+
+    [Fact]
+    public void ToAdSearchCriteriaDto_WhenCalledOnResultOfToBase64String_ShouldCorrectlyDeserialize()
+    {
+        // Arrange
+        AdSearchCriteriaDto expected = _fixture.Create<AdSearchCriteriaDto>();
+        string serialized = expected.ToBase64String();
+
+        // Act
+        AdSearchCriteriaDto? actual = serialized.ToAdSearchCriteriaDto();
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Equivalent(expected, actual);
+    }
+
+    [Fact]
+    public void ToSearchCriteriaDto_WhenCalledOnRandomString_ShouldReturnNull()
+    {
+        // Arrange
+        string input = _fixture.Create<string>();
+
+        // Act
+        AdSearchCriteriaDto? actual = input.ToAdSearchCriteriaDto();
+
+        Assert.Null(actual);
+    }
+}
---
+++ TutorLizard.Web/Extensions/DtoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using TutorLizard.BusinessLogic.Models.DTOs;
+
+namespace TutorLizard.Web.Extensions;
+
+public static class DtoExtensions
+{
+    public static string ToBase64String(this AdSearchCriteriaDto searchCriteria)
+    {
+        byte[] bytes = JsonSerializer.SerializeToUtf8Bytes(searchCriteria);
+        return Convert.ToBase64String(bytes);
+    }
+
+    public static AdSearchCriteriaDto? ToAdSearchCriteriaDto(this string base64String)
+    {
+        try
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+            return JsonSerializer.Deserialize<AdSearchCriteriaDto>(bytes);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
