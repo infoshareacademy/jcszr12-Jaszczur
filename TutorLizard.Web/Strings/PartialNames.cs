fcefafa558f85fde14a946d36ff3ca7eb1bd98ac
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-18 14:19:56 +0200

    Remove deleted partial view's name from PartialNames

--- TutorLizard.Web/Strings/PartialNames.cs
+++ TutorLizard.Web/Strings/PartialNames.cs
@@ -8,5 +8,4 @@ public static class PartialNames
     public const string PendingAdRequestsListItem = "_PendingAdRequestsListItem";
     public const string TutorAllAdRequestsListItem = "_TutorAllAdRequestsListItem";
     public const string AdSearchCriteria = "_AdSearchCriteria";
-    public const string GoToAdsPage = "_GoToAdsPage";
 }
