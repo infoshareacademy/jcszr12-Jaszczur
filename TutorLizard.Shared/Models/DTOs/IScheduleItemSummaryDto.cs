7a736f4180d36852d5ef3dc0ec344fc15487810c
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-27 14:51:32 +0200

    Add basic structure of the Calendar component

---
+++ TutorLizard.Shared/Models/DTOs/IScheduleItemSummaryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TutorLizard.Shared.Models.DTOs;
+public interface IScheduleItemSummaryDto
+{
+    int AdId { get; set; }
+    string AdTitle { get; set; }
+    DateTime DateTime { get; set; }
+    int Id { get; set; }
+}
