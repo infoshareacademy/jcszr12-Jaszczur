e73bbae3c2e8ac1b4cb017388985e8bd88125bbe
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-07-02 11:15:09 +0200

    Add logging in UserService

--- TutorLizard.BusinessLogic/Services/UserService.cs
+++ TutorLizard.BusinessLogic/Services/UserService.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using TutorLizard.Shared.Models.DTOs;
-using TutorLizard.BusinessLogic.Models;
-using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
-using TutorLizard.BusinessLogic.Extensions;
-using TutorLizard.BusinessLogic.Interfaces.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TutorLizard.BusinessLogic.Extensions;
+using TutorLizard.BusinessLogic.Interfaces.Data.Repositories;
+using TutorLizard.BusinessLogic.Interfaces.Services;
+using TutorLizard.BusinessLogic.Models;
 using TutorLizard.Shared.Enums;
+using TutorLizard.Shared.Models.DTOs;
 
 namespace TutorLizard.BusinessLogic.Services;
 
@@ -13,71 +14,95 @@ public class UserService : IUserService
 {
     private readonly PasswordHasher<User> _passwordHasher = new PasswordHasher<User>();
     private readonly IDbRepository<User> _userRepository;
+    private readonly ILogger<UserService> _logger;
 
-    public UserService(IDbRepository<User> userRepository)
+    public UserService(IDbRepository<User> userRepository,
+                       ILogger<UserService> logger)
     {
         _userRepository = userRepository;
+        _logger = logger;
     }
 
     public async Task<LogInResult> LogIn(string username, string password)
     {
+        using var scope = _logger.BeginMethodCallScope(nameof(LogIn), "LogIn with password request");
+
         var user = await _userRepository.GetAll()
             .FirstOrDefaultAsync(user => user.Name == username);
 
         if (user == null)
         {
-            return new LogInResult
+            _logger.LogWarning("User not found");
+            LogInResult userNotFoundResponse = new()
             {
                 ResultCode = LogInResultCode.UserNotFound
             };
+            _logger.LogReturningResponse(userNotFoundResponse);
+            return userNotFoundResponse;
         }
 
         if (!user.IsActive)
         {
-            return new LogInResult
+            _logger.LogWarning("User account not active");
+            LogInResult inactiveAccountResponse = new()
             {
                 ResultCode = LogInResultCode.InactiveAccount
             };
+            _logger.LogReturningResponse(inactiveAccountResponse);
+            return inactiveAccountResponse;
         }
 
-        var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, password);
+        var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash ?? "", password);
 
         if (result == PasswordVerificationResult.Success)
         {
-            return new LogInResult
+            LogInResult response = new()
             {
                 ResultCode = LogInResultCode.Success,
                 User = user.ToDto()
             };
+            _logger.LogReturningResponse(response, destructureResponse: true);
+            return response;
         }
 
-        return new LogInResult
+        _logger.LogWarning("Invalid password.");
+        LogInResult failedResponse = new()
         {
             ResultCode = LogInResultCode.InvalidPassword
         };
-    }    
-    
+        _logger.LogReturningResponse(failedResponse);
+        return failedResponse;
+    }
+
     public async Task<UserDto?> LogInWithGoogle(string username, string googleId)
     {
+        using var scope = _logger.BeginMethodCallScope(nameof(LogInWithGoogle), "LogIn with Google request");
+
         var user = await _userRepository.GetAll()
             .FirstOrDefaultAsync(user => user.GoogleId == googleId);
 
         if (user == null)
         {
+            _logger.LogWarning("User not found");
             return null;
         }
 
-        if (user.GoogleId == googleId)
-            return user.ToDto();
-
-        return null;
+        UserDto response = user.ToDto();
+        _logger.LogReturningResponse(response, destructureResponse: true);
+        return response;
     }
-
 
     public async Task<bool> RegisterUser(string userName, UserType type, string email, string password, string activationCode)
     {
+        using var scope = _logger.BeginMethodCallScope(nameof(RegisterUser), "Register user request");
+
         if (await _userRepository.GetAll().AnyAsync(user => user.Name == userName))
-            return false;
+        {
+            _logger.LogWarning("User with provided name already exists. User will not be created.");
+            bool failedResponse = false;
+            _logger.LogReturningResponse(failedResponse);
+            return failedResponse;
+        }
 
         User user = new()
         {
@@ -93,13 +118,22 @@ public class UserService : IUserService
 
         await _userRepository.Create(user);
 
-        return true;
+        bool response = true;
+        _logger.LogReturningResponse(response);
+        return response;
     }
 
     public async Task<bool> RegisterUserWithGoogle(string username, string email, string googleId)
     {
+        using var scope = _logger.BeginMethodCallScope(nameof(RegisterUserWithGoogle), "Register user with Google request");
+
         if (await _userRepository.GetAll().AnyAsync(user => user.Name == username))
-            return false;
+        {
+            _logger.LogWarning("User with provided name already exists. User will not be created.");
+            bool failedResponse = false;
+            _logger.LogReturningResponse(failedResponse);
+            return failedResponse;
+        }
 
         User user = new()
         {
@@ -111,44 +145,56 @@ public class UserService : IUserService
 
         await _userRepository.Create(user);
 
-        return true;
+        bool response = true;
+        _logger.LogReturningResponse(response);
+        return response;
     }
 
     public async Task<bool> IsTheGoogleUserRegistered(string googleId)
     {
-        if (await _userRepository.GetAll().AnyAsync(user => user.GoogleId == googleId)) 
-            return true;
+        using var scope = _logger.BeginMethodCallScope(nameof(IsTheGoogleUserRegistered), "Is The Google User Registered request");
 
-        return false;
+        bool response = false;
+
+        if (await _userRepository.GetAll().AnyAsync(user => user.GoogleId == googleId))
+            response = true;
+
+        _logger.LogReturningResponse(response);
+        return response;
     }
 
     public async Task<ActivationResultDto> ActivateUserAsync(string activationCode)
     {
-
+        using var scope = _logger.BeginMethodCallScope(nameof(ActivateUserAsync), activationCode);
         var user = await _userRepository.GetAll()
             .FirstOrDefaultAsync(u => u.ActivationCode == activationCode && u.IsActive == false);
 
-        if (user != null)
+        if (user is null)
         {
-            user.IsActive = true;
-            user.ActivationCode = "ACTIVATED";
-
-            await _userRepository.Update(user.Id, u =>
+            _logger.LogWarning("User with provided ActivationCode not found.");
+            ActivationResultDto failedResponse = new()
             {
-                u.IsActive = user.IsActive;
-                u.ActivationCode = user.ActivationCode;
-            });
-
-            return new ActivationResultDto
-            {
-                IsActivated = true,
+                IsActivated = false,
                 ActivationCode = activationCode
             };
+            _logger.LogReturningResponse(failedResponse);
+            return failedResponse;
         }
-        return new ActivationResultDto
+        user.IsActive = true;
+        user.ActivationCode = "ACTIVATED";
+
+        await _userRepository.Update(user.Id, u =>
         {
-            IsActivated = false,
+            u.IsActive = user.IsActive;
+            u.ActivationCode = user.ActivationCode;
+        });
+
+        ActivationResultDto response = new()
+        {
+            IsActivated = true,
             ActivationCode = activationCode
         };
+        _logger.LogReturningResponse(response);
+        return response;
     }
 }
