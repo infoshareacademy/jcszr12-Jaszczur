3b840648ebbf95c4b17b8955cf44d6a1bf6fb172
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-07-02 09:41:36 +0200

    Add LoggingExtensions

---
+++ TutorLizard.BusinessLogic/Extensions/LoggingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Core;
+using Microsoft.Extensions.Logging;
+
+namespace TutorLizard.BusinessLogic.Extensions;
+internal static class LoggingExtensions
+{
+    internal static IDisposable? BeginMethodCallScope<TService, TRequest>(this ILogger<TService> logger, string methodName, TRequest request, bool destructureRequest = true)
+    {
+        string scopeMessageFormat = $"Service: {{Service}} Method: {{Method}} Request: {(destructureRequest ? "{@Request}" : "{Request}")}";
+        var scope = logger.BeginScope(scopeMessageFormat, typeof(TService).Name, methodName, request);
+
+        string logMessageFormat = $"Service method call - Service: {{Service}} Method: {{Method}} Request: {(destructureRequest ? "{@Request}" : "{Request}")}";
+        logger.LogInformation(logMessageFormat, typeof(TService).Name, methodName, request);
+
+        return scope;
+    }
+
+    internal static void LogReturningResponse<TService, TResponse>(this ILogger<TService> logger, TResponse response, bool destructureResponse = true)
+    {
+        string logMessageFormat = $"Returning Response: {(destructureResponse ? "{@Response}" : "{Response}")}";
+        logger.LogInformation(logMessageFormat, response);
+    }
+}
