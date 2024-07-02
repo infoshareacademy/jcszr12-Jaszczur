97bd90446caf59416bc3acc049699379a412232f
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-07-02 12:06:32 +0200

    Modify log message format

--- TutorLizard.BusinessLogic/Extensions/LoggingExtensions.cs
+++ TutorLizard.BusinessLogic/Extensions/LoggingExtensions.cs
@@ -6,10 +6,10 @@ internal static class LoggingExtensions
 {
     internal static IDisposable? BeginMethodCallScope<TService, TRequest>(this ILogger<TService> logger, string methodName, TRequest request, bool destructureRequest = true)
     {
-        string scopeMessageFormat = $"Service: {{Service}} Method: {{Method}} Request: {(destructureRequest ? "{@Request}" : "{Request}")}";
+        string scopeMessageFormat = $"Service: {{Service}} Method: {{ServiceMethod}} Request: {(destructureRequest ? "{@Request}" : "{Request}")}";
         var scope = logger.BeginScope(scopeMessageFormat, typeof(TService).Name, methodName, request);
 
-        string logMessageFormat = $"Service method call - Service: {{Service}} Method: {{Method}} Request: {(destructureRequest ? "{@Request}" : "{Request}")}";
+        string logMessageFormat = $"[Service method call] Service: {{Service}} Method: {{ServiceMethod}} Request: {(destructureRequest ? "{@Request}" : "{Request}")}";
         logger.LogInformation(logMessageFormat, typeof(TService).Name, methodName, request);
 
         return scope;
@@ -17,7 +17,7 @@ internal static class LoggingExtensions
 
     internal static void LogReturningResponse<TService, TResponse>(this ILogger<TService> logger, TResponse response, bool destructureResponse = true)
     {
-        string logMessageFormat = $"Returning Response: {(destructureResponse ? "{@Response}" : "{Response}")}";
+        string logMessageFormat = $"[Returning response] Response: {(destructureResponse ? "{@Response}" : "{Response}")}";
         logger.LogInformation(logMessageFormat, response);
     }
 }
