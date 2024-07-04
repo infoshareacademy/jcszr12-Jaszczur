9b4b014afbad69bb257de5926d199607952e032b
GrzegorzKmiecinski <kmiecinskigrzegorz@gmail.com>
2024-07-04 15:15:27 +0200

    Change resource name

--- TutorLizard.Web/Resources/Shared.Designer.cs
+++ TutorLizard.Web/Resources/Shared.Designer.cs
@@ -22,14 +22,14 @@ namespace TutorLizard.Web.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resource {
+    public class Shared {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resource() {
+        internal Shared() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace TutorLizard.Web.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TutorLizard.Web.Resources.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TutorLizard.Web.Resources.Shared", typeof(Shared).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
