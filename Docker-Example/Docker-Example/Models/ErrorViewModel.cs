42be9233ba1dc3e915be54e6c283107839ab3578
Grzegorz <kmiecinskigrzegorz@gmail.com>
2024-07-04 14:26:25 +0200

    Add basic setup and dropdown menu to change languages

---
+++ Docker-Example/Docker-Example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace docker_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
